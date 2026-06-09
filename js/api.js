949bf4c82054e4bb1a4f177c05a1172ed94f1210
edusalman <ehnsalman@gmail.com>
2026-06-08 22:14:54 -0300

    mudança de 50 produtos para 25

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 async function carregarProdutos() {
     try {
-        const busca = await fetch('https://dummyjson.com/products?limit=50')
+        const busca = await fetch('https://dummyjson.com/products?limit=25')
         const dados = await busca.json()
         return dados.products
     } catch (erro) {
